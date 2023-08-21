307c337d918a4d0e382ea268b0ca794e2c1b80b4
mucahitemrekenan <mekenan.ds@gmail.com>
2023-08-21 17:55:18 +0300

    clustering and preprocessing questions added

--- clustering.py
+++ clustering.py
@@ -1,3 +1,49 @@
+"""
+Subtask 6.1: Explain the concepts of clustering methods, specifically K-means and Hierarchical clustering.
+Discuss their advantages and disadvantages, and where they might be used.
+
+My Answer:
+Clustering is a method that divides the samples into groups depending on their similarities.
+These similarities are calculated with distance, density, hierarchical, grid-based, neural network based, etc.
+Clustering methods are used in grouping the customers of a company like Netflix, Facebook, and Google.
+Netflix recommends new series to the customers depending on their calculated group.
+Facebook and Google show related ads to the customer.
+K-Means, Hierarchical, and DBSCAN are some of the clustering methods.
+K-Means is a distance-based method. It selects K random centers and assigns the nearest sample points to that cluster.
+Then calculates each cluster center again by meaning the sample points,
+and assigns the nearest sample points to new cluster centers.
+These center calculation and point assignment continue until the centers don’t move.
+Hierarchical clustering uses dendrogram, a tree divides the data based on distance.
+It splits the data into two with one of these distance metrics Euclidian, Manhattan, and cosine.
+Then split the clusters again and again until there is no rest of the data can be split.
+It has also linkage criteria for data points in a cluster. Single, complete, average,
+and ward linkage determines data density in a cluster.
+DBSCAN has two parameters epsilon and min points. Epsilon is the radius of the neighborhood around a data point.
+Minpts are the minimum number of points required to form a dense region.
+DBSCAN detects core points randomly which meet the epsilon and Minpts criteria.
+Then expands the cluster until border points.
+A border point is a point that meets epsilon criteria while it doesn’t meet the Minpts criteria.
+All the connected core points are merged in a cluster. These steps are repeated until all core points have been visited.
+
+Subtask 6.2: Choose a real or hypothetical problem and describe how you would apply clustering methods to solve it.
+Include details on the specific algorithms you would use and why.
+
+My Answer:
+We select the diabetes data in scikit-learn to apply the clustering methods of K-means and DBSCAN.
+I selected these methods because k-means calculate clusters with distance while DBSCAN uses linkage,
+and density ignoring noise points.
+Our data has different parameters from 414 diabetes patients.
+All the parameters are numerical and scaled to 0-1.
+This is important because the scale differences between features affect the DBSCAN algorithm negatively.
+K-means is more robust to scale differences, but it has been affected as well.
+We trained the scaled data with DBSCAN and K-means respectively.
+According to plots, it is obvious that DBSCAN can’t split accurately the target variable into groups.
+But the K-means split the target better.
+
+Subtask 6.3: Implement a Python code snippet that demonstrates the use of a clustering method on a sample dataset.
+You can use libraries like Scikit-learn for this purpose.
+"""
+
 import numpy as np
 import pandas as pd
 from lightgbm.sklearn import LGBMClassifier
--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,52 @@
+"""
+Subtask 5.1: Explain the importance of data preprocessing and feature engineering in machine learning.
+Discuss common techniques such as normalization, scaling, handling missing values, and encoding categorical variables.
+
+My Answer:
+There is a diagram on the internet
+that shows most of the effort in a data science project belongs to preprocessing and cleaning operations.
+And modeling is the smallest part of projects. In real life,
+the data is never ready for modeling and need to be processed to a convenient model.
+Feature engineering is the next part after preprocessing in data science projects.
+We generate new features from data or merge new features from outsources.
+This operation is a need to build stronger models.
+For example, in energy consumption prediction,
+we use not only the historical consumption data but also the weather data as well.
+Because the consumption can vary depending on temperature, rain, snow, or wind speed.
+In preprocessing, we normalize the numerical data. Because most of our models use linear methods in the backend.
+If the data has bad distribution, this harms the model equations and gives poor learning scores.
+So, we need data with convenient standard deviation and variance.
+Scaling is also an important process because of the same reasons as normalization.
+Generally, the 0-1 range is good for scaling the data, especially in deep learning.
+Our data has missing values all the time because of many reasons.
+There could be a data loss, or the data-collecting device may be out of service for that time.
+We can fill these missing values with lots of methods or drop these samples from the data simply.
+The data can be numerical and categorical as well.
+For categorical data, we encode the data as many as the number of categories.
+For linear-based models, this is a must. For nonlinear models like decision trees, encoding is not a must.
+
+Subtask 5.2: Choose a dataset (real or hypothetical),
+and describe how you would preprocess it for a specific machine learning task.
+Include details on the techniques you would apply and why.
+
+My Answer:
+I chose the iris dataset. This dataset includes eye colors and four axes length of these eyes.
+I will use multilayer perceptron aka neural networks classifier for the classification task.
+The data is cleaned already. This means the cleaning and missing value imputation processes will be skipped.
+We have four numerical features and one categorical target variable which has three classes.
+First, I scaled the numerical features in to a range of zero and one.
+Because I will use neural network, and they are so sensitive to beyond 0-1 range.
+I want to improve my learning accuracy. Standard deviation and variance should be adjusted,
+and there comes the standard scaler to help. It makes our standard deviation 1 and mean 0.
+In neural networks, categorical variables should be converted into one-hot encoded format.
+I used one-hot encoder for this task.
+I divided my dataset into train and test datasets to evaluate the model better.
+Finally, MLPClassifier from scikit-learn helps us to build the model.
+
+
+Subtask 5.3: Implement a Python code snippet that demonstrates one or more preprocessing techniques on a sample dataset.
+You can use libraries like Scikit-learn for this purpose.
+"""
 import numpy as np
 import pandas as pd
 from lightgbm.sklearn import LGBMClassifier
